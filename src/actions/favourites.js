43d355f30195364e62bc257538a792b0f5f30281
Kostas Kryarov <kvkryarov@avito.ru>
2018-04-21 21:41:50 +0300

    Added jsdoc

--- src/actions/favourites.js
+++ src/actions/favourites.js
@@ -1,5 +1,11 @@
 import { UPDATE_FAVOURITE_REQUEST } from '../constants/action-types';
 
+/**
+ * Update favourite flag in item
+ *
+ * @param {number} itemId
+ * @returns {Object} redux action
+ */
 export function updateFavourite(itemId) {
     return {
         type: UPDATE_FAVOURITE_REQUEST,
--- src/actions/items.js
+++ src/actions/items.js
@@ -1,5 +1,10 @@
 import { FETCH_ITEMS_REQUEST } from '../constants/action-types';
 
+/**
+ * Fetch items
+ *
+ * @returns {Object} redux action
+ */
 export function fetchItems() {
     return {
         type: FETCH_ITEMS_REQUEST
